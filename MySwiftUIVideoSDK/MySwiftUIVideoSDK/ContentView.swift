6f7f26e6629eecc90f61ed241e2b2a2d24a75603
Richard Yeh <richard.yeh@zoom.us>
2025-05-28 23:29:25 -0700

    SwiftUI app

---
+++ MySwiftUIVideoSDK/MySwiftUIVideoSDK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySwiftUIVideoSDK
+//
+//  Created by Richard Yeh on 5/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
